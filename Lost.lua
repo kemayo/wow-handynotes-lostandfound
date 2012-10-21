b478ae4f4a88aec1d6fc063c8f6094ef09211272
David Lynch <kemayo@gmail.com>
2012-10-20 22:20:58 -0500

    Use the flag quests to only show unfound items

--- Lost.lua
+++ Lost.lua
@@ -14,6 +14,7 @@ local db
 local defaults = {
     profile = {
         achievement_only = true,
+        found = false,
         icon_scale = 1.0,
         icon_alpha = 1.0,
         icon_item = true,
@@ -37,98 +38,108 @@ local points = {
     -- [mapFile] = { [coord] = { type=[type], id=[id], junk=[bool], }, }
     -- [] = { type="item", id=, }, -- 
     ["KunLaiSummit"] = {
-        [52907140] = { type="item", id=86394, note="in the cave", }, -- Hozen Warrior Spear
-        [35207640] = { type="item", id=86125, note="on Frozen Trail Packer", }, -- Kafa Press
-        [74707490] = { type="plain", id="Sprite's Cloth Chest", },
-        [57507100] = { type="plain", id="Sprite's Cloth Chest", },
-        [71206260] = { type="item", id=88723, note="in Stash of Yaungol Weapons", }, -- Sturdy Yaungol Spear
-        [44705240] = { type="item", id=86393, }, -- Tablet of Ren Yun
-        [64204510] = { type="item", id=86471, junk=true, }, -- Ancient Mogu Tablet
-        [49405940] = { type="plain", id="Hozen Treasure Cache", junk=true, },
-        [51802580] = { type="plain", id="Hozen Treasure Cache", junk=true, },
-        [36707980] = { type="plain", id="Lost Adventurer's Belongings", junk=true, },
-        [47007300] = { type="plain", id="Mo-Mo's Treasure Chest", junk=true, },
-        [52605150] = { type="item", id=86430, junk=true, note="in Rikktik's Tiny Chest", }, -- Rikktik's Tick Remover
-        [72003390] = { type="item", id=86422, junk=true, }, -- Statue of Xuen
-        [56804890] = { type="plain", id="Stolen Sprite Treasure", junk=true, },
-        [41504360] = { type="plain", id="Stolen Sprite Treasure", junk=true, },
-        [32005020] = { type="plain", id="Stolen Sprite Treasure", junk=true, },
-        [58407350] = { type="item", id=86427, junk=true, }, -- Terracotta Head
+        [52907140] = { type="item", id=86394, note="in the cave", quest=31413, }, -- Hozen Warrior Spear
+        [35207640] = { type="item", id=86125, note="on Frozen Trail Packer", quest=31304, npc=64227, }, -- Kafa Press
+        [73107350] = { type="plain", id="Sprite's Cloth Chest", note="in the cave", quest=31412, },
+        [71206260] = { type="item", id=88723, note="in Stash of Yaungol Weapons", quest=31421, }, -- Sturdy Yaungol Spear
+        [44705240] = { type="item", id=86393, quest=31417, }, -- Tablet of Ren Yun
+        [64204510] = { type="item", id=86471, junk=true, quest=31420, }, -- Ancient Mogu Tablet
+        [50306170] = { type="plain", id="Hozen Treasure Cache", junk=true, note="in the cave", quest=31414, },
+        [36707980] = { type="plain", id="Lost Adventurer's Belongings", junk=true, quest=31418, },
+        [47007300] = { type="plain", id="Mo-Mo's Treasure Chest", junk=true, quest=31868, },
+        [52605150] = { type="item", id=86430, junk=true, note="in Rikktik's Tiny Chest", quest=31419, }, -- Rikktik's Tick Remover
+        [72003390] = { type="item", id=86422, junk=true, quest=31416, }, -- Statue of Xuen
+        [59405300] = { type="plain", id="Stolen Sprite Treasure", junk=true, note="in the cave", quest=31415, },
+        [58407350] = { type="item", id=86427, junk=true, quest=31422, }, -- Terracotta Head
+    },
+    ["KnucklethumpHole"] = { -- cave in Kun-Lai
+        [49405940] = { type="plain", id="Hozen Treasure Cache", junk=true, quest=31414, },
+    },
+    ["TheDeeper"] = { -- cave in Kun-Lai
+        [24106580] = { type="item", id=86394, level=12, quest=31413, }, -- Hozen Warrior Spear
+    },
+    ["PrankstersHollow"] = { -- cave in Kun-Lai
+        [54706980] = { type="plain", id="Sprite's Cloth Chest", quest=31412, },
+    },
+    ["HowlingwindCavern"] = { -- cave in Kun-Lai
+        [41404420] = { type="plain", id="Stolen Sprite Treasure", junk=true, quest=31415, },
     },
     ["TownlongWastes"] = {
-        [66304470] = { type="item", id=86518, }, -- Yaungol Fire Carrier
-        [66804800] = { type="item", id=86518, }, -- Yaungol Fire Carrier
-        [62803410] = { type="plain", id="Abandoned Crate of Goods", junk=true, },
-        [65808610] = { type="item", id=86472, junk=true, }, -- Amber Encased Moth
-        [52805620] = { type="item", id=86517, junk=true, }, -- Hardened Sap of Kri'vess
-        [57505850] = { type="item", id=86517, junk=true, }, -- Hardened Sap of Kri'vess
-        [32806160] = { type="item", id=86516, junk=true, note="in the cave", }, -- Fragment of Dread
+        [66304470] = { type="item", id=86518, quest=31425, }, -- Yaungol Fire Carrier
+        [66804800] = { type="item", id=86518, quest=31425, }, -- Yaungol Fire Carrier
+        [62803410] = { type="plain", id="Abandoned Crate of Goods", junk=true, note="in a tent", quest=31427, },
+        [65808610] = { type="item", id=86472, junk=true, quest=31426, }, -- Amber Encased Moth
+        [52805620] = { type="item", id=86517, junk=true, quest=31424, }, -- Hardened Sap of Kri'vess
+        [57505850] = { type="item", id=86517, junk=true, quest=31424, }, -- Hardened Sap of Kri'vess
+        [32806160] = { type="item", id=86516, junk=true, note="in the cave", quest=31423, }, -- Fragment of Dread
     },
-    ["NiuzaoCatacombs"] = {
-        [56406480] = { type="item", id=86516, junk=true, }, -- Fragment of Dread
-        [36908760] = { type="item", id=86516, junk=true, }, -- Fragment of Dread
-        [48408860] = { type="item", id=86516, junk=true, }, -- Fragment of Dread
-        [64502150] = { type="item", id=86516, junk=true, }, -- Fragment of Dread
+    ["NiuzaoCatacombs"] = { -- cave in Townlong
+        [56406480] = { type="item", id=86516, junk=true, quest=31423, }, -- Fragment of Dread
+        [36908760] = { type="item", id=86516, junk=true, quest=31423, }, -- Fragment of Dread
+        [48408860] = { type="item", id=86516, junk=true, quest=31423, }, -- Fragment of Dread
+        [64502150] = { type="item", id=86516, junk=true, quest=31423, }, -- Fragment of Dread
     },
     ["ValleyoftheFourWinds"] = {
-        [46802460] = { type="item", id=85973, note="on Ghostly Pandaren Fisherman", }, -- Ancient Pandaren Fishing Charm
-        [45403820] = { type="item", id=86079, note="on Ghostly Pandaren Craftsman", }, -- Ancient Pandaren Woodcutter
-        [15402920] = { type="item", id=86218, }, -- Staff of the Hidden Master
-        [14903360] = { type="item", id=86218, }, -- Staff of the Hidden Master
-        [17503570] = { type="item", id=86218, }, -- Staff of the Hidden Master
-        [19103780] = { type="item", id=86218, }, -- Staff of the Hidden Master
-        [19004250] = { type="item", id=86218, }, -- Staff of the Hidden Master
-        [43503740] = { type="plain", id="Cache of Pilfered Goods", },
-        [92003900] = { type="item", id=87524, junk=true, }, -- Boat-Building Instructions
-        [23802840] = { type="plain", id="Virmen Treasure Cache", junk=true, },
+        [46802460] = { type="item", id=85973, note="on Ghostly Pandaren Fisherman", npc=64004, quest=31284, }, -- Ancient Pandaren Fishing Charm
+        [45403820] = { type="item", id=86079, note="on Ghostly Pandaren Craftsman", npc=64191, quest=31292, }, -- Ancient Pandaren Woodcutter
+        [15402920] = { type="item", id=86218, quest=31407, }, -- Staff of the Hidden Master
+        [14903360] = { type="item", id=86218, quest=31407, }, -- Staff of the Hidden Master
+        [17503570] = { type="item", id=86218, quest=31407, }, -- Staff of the Hidden Master
+        [19103780] = { type="item", id=86218, quest=31407, }, -- Staff of the Hidden Master
+        [19004250] = { type="item", id=86218, quest=31407, }, -- Staff of the Hidden Master
+        [43603740] = { type="plain", id="Cache of Pilfered Goods", quest=31406, },
+        [92003900] = { type="item", id=87524, junk=true, quest=31869, }, -- Boat-Building Instructions
+        [23802840] = { type="plain", id="Virmen Treasure Cache", junk=true, quest=31405, },
     },
     ["DreadWastes"] = {
-        [66306660] = { type="item", id=86522, }, -- Blade of the Prime
-        [25905030] = { type="item", id=86525, }, -- Bloodsoaked Chitin Fragment
-        [30209080] = { type="item", id=86524, }, -- Dissector's Staff of Mutation
-        [33003010] = { type="item", id=86521, }, -- Lucid Amulet of the Agile Mind
-        [48703000] = { type="item", id=86520, }, -- Malik's Stalwart Spear
-        [42206360] = { type="item", id=86529, note="on Glinting Rapana Whelk", }, -- Manipulator's Talisman on a Glinting Rapana Whelk (65552)
-        [56607780] = { type="item", id=86523, }, -- Swarming Cleaver of Ka'roz
-        [71803610] = { type="item", id=86519, }, -- Wind-Reaver's Dagger of Quick Strikes
-        [28804190] = { type="item", id=86527, }, -- Blade of the Poisoned Mind
+        [66306660] = { type="item", id=86522, quest=31433, }, -- Blade of the Prime
+        [25905030] = { type="item", id=86525, quest=31436, }, -- Bloodsoaked Chitin Fragment
+        [30209080] = { type="item", id=86524, quest=31435, }, -- Dissector's Staff of Mutation
+        [33003010] = { type="item", id=86521, quest=31431, }, -- Lucid Amulet of the Agile Mind
+        [48703000] = { type="item", id=86520, quest=31430, }, -- Malik's Stalwart Spear
+        [42206360] = { type="item", id=86529, note="on Glinting Rapana Whelk", npc=65552, quest=31432, }, -- Manipulator's Talisman on a Glinting Rapana Whelk (65552)
+        [56607780] = { type="item", id=86523, quest=31434, }, -- Swarming Cleaver of Ka'roz
+        [54305650] = { type="item", id=86526, quest=31437, }, -- Swarmkeeper's Medallion
+        [71803610] = { type="item", id=86519, quest=31429, }, -- Wind-Reaver's Dagger of Quick Strikes
+        [28804190] = { type="item", id=86527, quest=31438, }, -- Blade of the Poisoned Mind
     },
     ["Krasarang"] = {
-        [42309200] = { type="plain", id="Equipment Locker", },
-        [52308870] = { type="item", id=87266, note="in a barrel" }, -- Recipe: Banana Infused Rum
-        [50804930] = { type="item", id=86124, }, -- Pandaren Fishing Spear
-        [52007300] = { type="item", id=87798, junk=true, }, -- Stack of Papers
+        [42409200] = { type="plain", id="Equipment Locker", quest=31410, },
+        [52308870] = { type="item", id=87266, note="in a barrel", quest=31411, }, -- Recipe: Banana Infused Rum
+        [50804930] = { type="item", id=86124, quest=31409, }, -- Pandaren Fishing Spear
+        [52007300] = { type="item", id=87798, junk=true, quest=31863, }, -- Stack of Papers
+        [71000920] = { type="item", id=86220, junk=true, note="in the cave", quest=31408, }, -- Saurok Stone Tablet
     },
     ["ValeofEternalBlossoms"] = {
         -- nothing?
     },
     ["TheJadeForest"] = {
-        [39400730] = { type="item", id=85776, note="in the well", }, -- Wodin's Mantid Shaker
-        [39264665] = { type="item", id=86199, note="on Jade Warrior Statue", }, -- Jade Infused Blade
-        [43001160] = { type="item", id=86198, }, -- Hammer of Ten Thunders
-        [41801760] = { type="item", id=86198, }, -- Hammer of Ten Thunders
-        [46102920] = { type="item", id=85777, note="in the cave", }, -- Ancient Pandaren Mining Pick
-        [47106740] = { type="item", id=86196, }, --Ancient Jinyu Staff
-        [46207120] = { type="item", id=86196, }, --Ancient Jinyu Staff
-        [26203240] = { type="item", id=85780, junk=true, }, -- Ancient Pandaren Tea Pot
-        [31902780] = { type="item", id=85781, junk=true, }, -- Lucky Pandaren Coin
-        [23503500] = { type="item", id=86216, junk=true, }, -- Pandaren Ritual Stone
+        [39400730] = { type="item", id=85776, note="in the well", quest=31397, }, -- Wodin's Mantid Shaker
+        [39264665] = { type="item", id=86199, note="on Jade Warrior Statue", npc=64272, quest=31307, }, -- Jade Infused Blade
+        [43001160] = { type="item", id=86198, quest=31403, }, -- Hammer of Ten Thunders
+        [41801760] = { type="item", id=86198, quest=31403, }, -- Hammer of Ten Thunders
+        [41201390] = { type="item", id=86198, quest=31403, }, -- Hammer of Ten Thunders
+        [46102920] = { type="item", id=85777, note="in the cave", quest=31399, }, -- Ancient Pandaren Mining Pick
+        [47106740] = { type="item", id=86196, quest=31402, }, --Ancient Jinyu Staff
+        [46207120] = { type="item", id=86196, quest=31402, }, --Ancient Jinyu Staff
+        [26203240] = { type="item", id=85780, junk=true, quest=31400, }, -- Ancient Pandaren Tea Pot
+        [31902780] = { type="item", id=85781, junk=true, quest=31401, }, -- Lucky Pandaren Coin
+        [23503500] = { type="item", id=86216, junk=true, quest=31404, }, -- Pandaren Ritual Stone
+        [24005300] = { type="plain", id="Chest of Supplies", junk=true, quest=31864, },
+        [46308070] = { type="plain", id="Offering of Rememberance", junk=true, quest=31865, },
+        [70007400] = { type="plain", id="Ship's Locker", junk=true, quest=31396, },
+        [62452752] = { type="plain", id="Stash of Gems", junk=true, quest=31866, },
+    },
+    ["GreenstoneQuarry"] = { -- cave in Jade Forest
+        [33107800] = { type="item", id=85777, level=7, quest=31399, }, -- Ancient Pandaren Mining Pick
+        [44007050] = { type="item", id=85777, level=7, quest=31399, }, -- Ancient Pandaren Mining Pick
+        [43703850] = { type="item", id=85777, level=7, quest=31399, }, -- Ancient Pandaren Mining Pick
+        [38704750] = { type="item", id=85777, level=7, quest=31399, }, -- Ancient Pandaren Mining Pick
+        [32606270] = { type="item", id=85777, level=7, quest=31399, }, -- Ancient Pandaren Mining Pick
     },
     ["TheHiddenPass"] = {
-        [74907670] = { type="item", id=86473, junk=true, }, -- The Hammer of Folly
-        [55107200] = { type="plain", id="Forgotten Lockbox", junk=true, },
-    },
-    -- cave in Kun-Lai
-    ["TheDeeper"] = {
-        [24106580] = { type="item", id=86394, level=12, }, -- Hozen Warrior Spear
-    },
-    -- cave in Jade Forest
-    ["GreenstoneQuarry"] = {
-        [33107800] = { type="item", id=85777, level=7, }, -- Ancient Pandaren Mining Pick
-        [44007050] = { type="item", id=85777, level=7, }, -- Ancient Pandaren Mining Pick
-        [43703850] = { type="item", id=85777, level=7, }, -- Ancient Pandaren Mining Pick
-        [38704750] = { type="item", id=85777, level=7, }, -- Ancient Pandaren Mining Pick
-        [32606270] = { type="item", id=85777, level=7, }, -- Ancient Pandaren Mining Pick
+        [74907670] = { type="item", id=86473, junk=true, quest=31428, }, -- The Hammer of Folly
+        [55107200] = { type="plain", id="Forgotten Lockbox", junk=true, quest=31867, },
     },
 }
 
@@ -161,7 +172,7 @@ local get_point_info = function(point)
         if point.note then
             label = ("%s (%s)"):format(label, point.note)
         end
-        return label, icon, point.junk
+        return label, icon, point.junk, point.quest
     end
 end
 local get_point_info_by_coord = function(mapFile, coord)
@@ -268,9 +279,9 @@ do
         while state do -- Have we reached the end of this zone?
             if value then
                 if not value.level or value.level == currentLevel then
-                    local label, icon, junk = get_point_info(value)
+                    local label, icon, junk, quest = get_point_info(value)
                     Debug("iter step", state, icon, db.icon_scale, db.icon_alpha)
-                    if not (junk and db.achievement_only) then
+                    if not ((junk and db.achievement_only) or (not db.found and quest and IsQuestFlaggedCompleted(quest))) then
                         return state, nil, icon, db.icon_scale, db.icon_alpha
                     end
                 end
@@ -326,6 +337,11 @@ local options = {
             name = "Achievement only",
             desc = "Only show the items which count for the Lost and Found achievement (skips some vendorable greys).",
         },
+        found = {
+            type = "toggle",
+            name = "Show found",
+            desc = "Show waypoints for iteme you've already found?",
+        },
     },
 }
 
@@ -339,13 +355,6 @@ function HL:OnInitialize()
     db = self.db.profile
     -- Initialize our database with HandyNotes
     HandyNotes:RegisterPluginDB("LostAndFound", HLHandler, options)
-end
-
-function HL:OnEnable()
-    -- self:RegisterEvent("CRITERIA_UPDATE", "Refresh")
-    -- self:RegisterEvent("CRITERIA_EARNED", "Refresh")
-    -- self:RegisterEvent("CRITERIA_COMPLETE", "Refresh")
-    -- self:RegisterEvent("ACHIEVEMENT_EARNED", "Refresh")
 end
 
 function HL:Refresh()
