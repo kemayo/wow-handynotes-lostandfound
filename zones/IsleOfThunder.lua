5bfd2cd942cd8619456a56854db78f13a4ba7511
David Lynch <kemayo@gmail.com>
2022-09-20 19:56:55 -0500

    Notes about isle of thunder questids

--- zones/IsleOfThunder.lua
+++ zones/IsleOfThunder.lua
@@ -1,28 +1,27 @@
 local myname, ns = ...
 
+-- quests:
+-- 32610 = got a Shan'ze Ritual Stone from a rare mob
+-- 32626 = got a Key to the Palace of Lei Shen from a mob drop
+
 ns.RegisterPoints(504, { -- Isle of Thunder
     [44603000] = { -- Al'tabim the All-Seeing
-        quest=nil,
         criteria=22826,
         npc=70000,
     },
     [48402540] = { -- Backbreaker Uru
-        quest=nil,
         criteria=22827,
         npc=70001,
     },
     [61404940] = { -- God-Hulk Ramuk
-        quest=nil,
         criteria=22825,
         npc=69999,
     },
     [53605300] = { -- Goda
-        quest=nil,
         criteria=22824,
         npc=69998,
     },
     [48208700] = { -- Haywire Sunreaver Construct
-        quest=nil,
         criteria=22820,
         npc=50358,
         loot={
@@ -30,27 +29,22 @@ ns.RegisterPoints(504, { -- Isle of Thunder
         },
     },
     [33408080] = { -- Ku'lai the Skyclaw +2
-        quest=nil,
         criteria=22822,
         npc=69996,
     },
     [54403560] = { -- Lu-Ban
-        quest=nil,
         criteria=22828,
         npc=70002,
     },
-    [59403640] = { -- Molthor +2
-        quest=nil,
+    [63444916] = { -- Molthor +2
         criteria=22829,
         npc=70003,
     },
     [35006220] = { -- Mumta
-        quest=nil,
         criteria=22821,
         npc=69664,
     },
     [50407220] = { -- Progenitus
-        quest=nil,
         criteria=22823,
         npc=69997,
     },
@@ -59,14 +53,12 @@ ns.RegisterPoints(504, { -- Isle of Thunder
 })
 ns.RegisterPoints(505, { -- Lightning Vein Mine
     [33802760] = { -- Backbreaker Uru +5
-        quest=nil,
         criteria=22827,
         npc=70001,
     },
 })
 ns.RegisterPoints(506, { -- The Swollen Vault
     [32003180] = { -- Molthor +6
-        quest=nil,
         criteria=22829,
         npc=70003,
     },
@@ -74,7 +66,7 @@ ns.RegisterPoints(506, { -- The Swollen Vault
 
 ns.RegisterPoints(504, { -- Isle of Thunder
     [60503730] = { -- Nalak
-        quest=32518,
+        quest=32518, -- also 33109?
         npc=69099,
         loot={
             {95057,mount=542,}, -- Reins of the Thundering Cobalt Cloud Serpent
@@ -83,7 +75,6 @@ ns.RegisterPoints(504, { -- Isle of Thunder
         atlas="VignetteKillElite", scale=1.2,
     },
     [39408140] = { -- Ra'sha
-        quest=nil,
         npc=70530,
         loot={
             95566, -- Ra'sha's Sacrificial Dagger
